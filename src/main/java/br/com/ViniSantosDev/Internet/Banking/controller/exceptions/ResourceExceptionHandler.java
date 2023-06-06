e7abf8b55ccf4018d06376f4832e9bdce9cab2e3
Vinicius Santos <viniciusilvasantos.contact@gmail.com>
2023-06-05 23:19:20 -0300

    implementar standard error e controller advice exceptions

---
+++ src/main/java/br/com/ViniSantosDev/Internet/Banking/controller/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,2 @@
+package br.com.ViniSantosDev.Internet.Banking.controller.exceptions;public class ResourceExceptionHandler {
+}
---
+++ src/main/java/br/com/ViniSantosDev/Internet/Banking/service/exceptions/DataBaseException.java
@@ -0,0 +1,2 @@
+package br.com.ViniSantosDev.Internet.Banking.service.exceptions;public class DataBaseException {
+}
